f33abc8cd1f74f0ab78007a3f9863460fa53dbc7
TheHBK <TheHBK@users.noreply.github.com>
2019-10-22 00:12:53 +0530

    Bootstrap Quantile estimation

--- codeSnippets/bootstrapMedian.R
+++ codeSnippets/bootstrapMedian.R
@@ -3,3 +3,11 @@
 x = faithful$waiting
 bootmed = apply(matrix(sample(x, rep=TRUE, 10^4*length(x)), nrow=10^4), 1, median)
 quantile(bootmed, c(.025, 0.975))
+quantile(bootmed, c(0.5, 0.995))
+quantile(bootmed, c(0.05, 0.5))
+sort(x)
+quantile(x)
+
+
+bootmean = apply(matrix(sample(x, rep=TRUE, 10^4*length(x)), nrow=10^4), 1, mean)
+quantile(bootmean, c(0.025, 0.975))
